59b3c820f17be91c68920960581d7c82c9a5ff1f
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2025-01-10 00:07:57 +0100

    Updated Base URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const baseURL = import.meta.env.NODE_ENV === "production" ? "https://xerostore-backend.onrender.com" : "http://localhost:3000"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:3000",
+      "/api/": baseURL,
     },
   },
 });
