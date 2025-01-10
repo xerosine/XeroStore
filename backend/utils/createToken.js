50215e4b9930b3eb1e6061c493f59045d258974e
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2025-01-10 21:39:28 +0100

    Update

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -6,8 +6,8 @@ const createToken = (res, userId) => {
   });
 
   res.cookie("jwt", token, {
-    httpOnly: true,
-    // secure: process.env.NODE_ENV !== "development",
+    // httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
     sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
     domain: '.onrender.com'
