58ec69e37f1d4c57f7fe9d08ce700cc823d8e988
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2025-01-10 23:02:26 +0100

    Update

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: ["https://xerostore.onrender.com/", "http://localhost:5173"],
+  origin: ["https://xerostore.onrender.com", "http://localhost:5173"],
   credentials: true
 }))
 
