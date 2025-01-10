fc68c6c154ed65fc130ad39ded4748d0a3cb21e7
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2025-01-10 15:17:47 +0100

    Updated cors credentials

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+  origin: 'https://xerostore.onrender.com',
+  credentials: true,
+}))
 
 app.use("/api/users", userRoutes);
 app.use("/api/categories", categoryRoutes);
