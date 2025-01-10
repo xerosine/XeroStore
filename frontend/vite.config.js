f1ca16e09bf89f83c044713ba799def84d0066a3
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2025-01-10 03:33:21 +0100

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const baseURL = process.env.NODE_ENV === "production" ? "https://xerostore-backend.onrender.com" : "http://localhost:3000"
-console.log(baseURL);
-
+console.log(process.env.NODE_ENV);
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://xerostore-backend.onrender.com"
+    : "http://localhost:3000";
 
 // https://vitejs.dev/config/
 export default defineConfig({
