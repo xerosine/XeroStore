ce243822b7b64b7e6a22679190f31acf5addd00d
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2025-01-10 15:09:00 +0100

    Update

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -8,7 +8,7 @@ const createToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
