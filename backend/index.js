c8db356be04113ef853104c938d37325e2d8d04a
Ayanga Oluwamurewa <ayangaoluwamurewa@gmail.com>
2025-01-10 23:14:38 +0100

    Update

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
 
 //utils and config
 import connectDB from "./config/db.js";
@@ -24,10 +24,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: ["https://xerostore.onrender.com", "http://localhost:5173"],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://xerostore.onrender.com"
+        : "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/categories", categoryRoutes);
